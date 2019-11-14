944d61c5abc5cd835ad62ed83c9420d2ab1b7d7e
Enric Leon <enricleonfont@gmail.com>
2019-11-14 18:47:07 +0100

    Add round to mark today

--- projects/simple-calendar-components/src/modules/core/models/calendar-day.ts
+++ projects/simple-calendar-components/src/modules/core/models/calendar-day.ts
@@ -1,7 +1,15 @@
+import * as moment_ from 'moment';
 import { Moment } from 'moment';
+
+const moment = moment_;
 
 export class CalendarDay {
     date: Moment;
     description: string;
     temperature: string;
+
+    isToday() {
+        var isCurrentDate = this.date.isSame(new Date(), "day");
+        return isCurrentDate;
+    }
 }
